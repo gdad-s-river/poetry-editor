ede86709ae4cbf382b5d0c0f10e8448d813804b0
gdad-s-river <arihantverma1994@gmail.com>
2017-10-20 00:34:31 +0530

    init commit (dropzone to drag and drop images)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ImageSelector from "./components/ImageSelector";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ImageSelector />
+      </div>
+    );
+  }
+}
+
+export default App;
